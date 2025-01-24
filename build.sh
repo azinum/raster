834ab1e9027cf255b901886cab844b45f669eb25
azinum <lukaska61@gmail.com>
2025-01-24 15:59:12 +0100

    Copy common.h, random.h, and stb_sprintf.h from deps/common.h

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -xe
 
 OPT=-O2
-clang --target=wasm32 -flto -ffast-math -nostdlib -Wl,--no-entry -Wl,--export-all -Wl,--allow-undefined ${OPT} -Ideps/common.h -Iinclude src/raster.c -o raster.wasm -DNO_STDLIB -DNO_STDIO -DTARGET_WASM -DNO_MATH -fvectorize
+clang --target=wasm32 -flto -ffast-math -nostdlib -Wl,--no-entry -Wl,--export-all -Wl,--allow-undefined ${OPT} -Iinclude src/raster.c -o raster.wasm -DNO_STDLIB -DNO_STDIO -DTARGET_WASM -DNO_MATH -fvectorize
 
 # wasm2wat raster.wasm -o raster.wat
