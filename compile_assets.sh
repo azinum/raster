618ef787999d6bf3123b6314b67fbed41b08e3a9
azinum <lukaska61@gmail.com>
2025-01-24 15:40:25 +0100

    Simplify tools building. Add ci (ubuntu 20.04).

--- compile_assets.sh
+++ compile_assets.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 FILE=include/assets.h
+TOOLS_PATH=$1
 
 echo "// assets.h" > ${FILE}
 echo "#ifndef _ASSETS_H" >> ${FILE}
@@ -9,21 +10,21 @@ echo "#define _ASSETS_H" >> ${FILE}
 for OBJ_PATH in data/mesh/*.obj; do
 	NAME=${OBJ_PATH%.obj}
 	NAME=${NAME##*/}
-	objtoc ${OBJ_PATH} ${NAME} >> ${FILE}
+	${TOOLS_PATH}objtoc ${OBJ_PATH} ${NAME} >> ${FILE}
 done
 
 for PNG_PATH in data/texture/*.png; do
 	NAME=${PNG_PATH%.png}
 	NAME=${NAME##*/}
-	pngtoc ${PNG_PATH} ${NAME} >> ${FILE}
+	${TOOLS_PATH}pngtoc ${PNG_PATH} ${NAME} >> ${FILE}
 done
 
 for TTF_PATH in data/font/*.ttf; do
 	NAME=${TTF_PATH%.ttf}
 	NAME=${NAME##*/}
-	font2c ${TTF_PATH} -n ${NAME} >> ${FILE}
+	${TOOLS_PATH}font2c ${TTF_PATH} -n ${NAME} >> ${FILE}
 done
 
-lutgen >> ${FILE}
+${TOOLS_PATH}lutgen >> ${FILE}
 
 echo "#endif // _ASSETS_H" >> ${FILE}
